043c8c9693e7033791a992d847ad1a7785bbd2f6
Kimi <porthan.kimi@gmail.com>
2022-11-22 14:09:09 +0200

    cors and generic request error type

--- pkg/api/ping.go
+++ pkg/api/ping.go
@@ -13,6 +13,7 @@ import (
 // @Router /ping [get]
 // @Success 200 {object} types.PingResponse
 func getPing(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	// Return status 200
 	w.WriteHeader(http.StatusOK)
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -3,3 +3,9 @@ package types
 type PingResponse struct {
 	Message string `json:"message"`
 }
+
+type RequestError struct {
+	StatusCode int    `json:"statusCode"`
+	Error      string `json:"error"`
+	Message    string `json:"message"`
+}
